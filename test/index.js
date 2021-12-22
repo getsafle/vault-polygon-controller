bda2b19aae21455de4f6354b3c5a1a8b1c620736
Suraj Singla <suraj.singla@mckinleyrice.co>
2021-12-22 12:04:43 +0530

    Update test cases

--- test/index.js
+++ test/index.js
@@ -1,30 +1,30 @@
 var assert = require('assert');
 const Web3 = require('web3')
-const bridgeContract = require('./contract-json/BridgeBsc.json');
+const bridgeContract = require('./contract-json/BridgePolygon.json');
 const CryptoJS = require('crypto-js');
-const BSCKeyring = require('../src/index')
+const PolygonKeyring = require('../src/index')
 const {
     HD_WALLET_12_MNEMONIC,
     HD_WALLET_12_MNEMONIC_TEST_OTHER,
     TESTING_MESSAGE_1,
     TESTING_MESSAGE_2,
     TESTING_MESSAGE_3,
-    BSC_NETWORK: {
+    POLYGON_NETWORK: {
         TESTNET,
         MAINNET
     },
-    TRANSFER_BSC: {
-        BSC_AMOUNT,
-        BSC_RECEIVER
+    TRANSFER_POLYGON: {
+        POLYGON_AMOUNT,
+        POLYGON_RECEIVER
     },
     CONTRACT_TXN: {
-        BSC_CONTRACT,
-        BSC_AMOUNT_TO_CONTRACT
+        POLYGON_CONTRACT,
+        POLYGON_AMOUNT_TO_CONTRACT
     },
 } = require('./constants');
 
 const CONTRACT_MINT_PARAM = {
-    from: BSC_CONTRACT,
+    from: POLYGON_CONTRACT,
     to: '', // this will be the current account 
     amount: 1,
     nonce: 0,
@@ -60,96 +60,50 @@ const PASSWORD = "random_password"
  */
 
 describe('Initialize wallet ', () => {
-    const bscKeyring = new BSCKeyring(opts)
+    const polygonKeyring = new PolygonKeyring(opts)
 
     it("Create new vault and keychain", async () => {
-        const res = await bscKeyring.createNewVaultAndKeychain(PASSWORD)
+        const res = await polygonKeyring.createNewVaultAndKeychain(PASSWORD)
         console.log("res ", res)
     })
 
     it("Create new vault and restore", async () => {
-        const res = await bscKeyring.createNewVaultAndRestore(PASSWORD, HD_WALLET_12_MNEMONIC)
-        assert(bscKeyring.keyrings[0].mnemonic === HD_WALLET_12_MNEMONIC, "Wrong mnemonic")
+        const res = await polygonKeyring.createNewVaultAndRestore(PASSWORD, HD_WALLET_12_MNEMONIC)
+        assert(polygonKeyring.keyrings[0].mnemonic === HD_WALLET_12_MNEMONIC, "Wrong mnemonic")
     })
 
     it("Export account (privateKey)", async () => {
-        const res = await bscKeyring.getAccounts()
+        const res = await polygonKeyring.getAccounts()
         let account = res[0]
-        const accRes = await bscKeyring.exportAccount(account)
+        const accRes = await polygonKeyring.exportAccount(account)
         console.log("accRes ", accRes, Buffer.from(accRes, 'hex'))
     })
 
     it("Get accounts", async () => {
-        const acc = await bscKeyring.getAccounts()
+        const acc = await polygonKeyring.getAccounts()
         console.log("acc ", acc)
     })
 
-    it("Sign transaction - transfer BSC", async () => {
-        const accounts = await bscKeyring.getAccounts()
-        const web3 = new Web3(TESTNET.URL);
-
-        console.log("accounts[0] ", accounts[0])
-
-        const tx = {
-            from: accounts[0],
-            to: BSC_RECEIVER,
-            value: BSC_AMOUNT
-        }
-
-        const signedTxn = await bscKeyring.signTransaction(tx, web3)
-        console.log("signedTxn ", signedTxn)
-
-        // const sentTxn = await bscKeyring.sendTransaction(signedTxn, web3)
-        // console.log("sentTxn ", sentTxn)
-    })
-
-    it("Sign Transaction - mint token in a demo token contract", async () => {
-
-        const accounts = await bscKeyring.getAccounts()
-        const web3 = new Web3(TESTNET.URL);
-
-        const bridgeBsc = new web3.eth.Contract(
-            bridgeContract.abi,
-            bridgeContract.networks[`${TESTNET.CHAIN_ID}`].address
-        );
-
-        const txData = bridgeBsc.methods.mint(CONTRACT_MINT_PARAM.from, accounts[0].toLowerCase(), CONTRACT_MINT_PARAM.amount, CONTRACT_MINT_PARAM.nonce, CONTRACT_MINT_PARAM.signature);
-        const data = txData.encodeABI();
-
-        const tx = {
-            from: accounts[0],
-            to: BSC_CONTRACT,
-            value: BSC_AMOUNT_TO_CONTRACT,
-            data
-        }
-
-        const signedTxn = await bscKeyring.signTransaction(tx, web3);
-        console.log("signedTxn ", signedTxn)
-
-        // const sentTxn = await bscKeyring.sendTransaction(signedTxn, web3)
-        // console.log("sentTxn ", sentTxn)
-    })
-
     it("Get fees", async () => {
-        const accounts = await bscKeyring.getAccounts()
+        const accounts = await polygonKeyring.getAccounts()
         const web3 = new Web3(TESTNET.URL);
 
-        const bridgeBsc = new web3.eth.Contract(
+        const bridgePolygon = new web3.eth.Contract(
             bridgeContract.abi,
             bridgeContract.networks[`${TESTNET.CHAIN_ID}`].address
         );
 
-        const txData = bridgeBsc.methods.mint(CONTRACT_MINT_PARAM.from, accounts[0].toLowerCase(), CONTRACT_MINT_PARAM.amount, CONTRACT_MINT_PARAM.nonce, CONTRACT_MINT_PARAM.signature);
+        const txData = bridgePolygon.methods.mint(CONTRACT_MINT_PARAM.from, accounts[0].toLowerCase(), CONTRACT_MINT_PARAM.amount, CONTRACT_MINT_PARAM.nonce, CONTRACT_MINT_PARAM.signature);
         const data = txData.encodeABI();
 
         const tx = {
             from: accounts[0],
-            to: BSC_CONTRACT,
-            value: BSC_AMOUNT_TO_CONTRACT,
+            to: POLYGON_CONTRACT,
+            value: POLYGON_AMOUNT_TO_CONTRACT,
             data
         }
 
-        const fees = await bscKeyring.getFees(tx, web3)
+        const fees = await polygonKeyring.getFees(tx, web3)
         console.log("fees ", fees)
 
     })
